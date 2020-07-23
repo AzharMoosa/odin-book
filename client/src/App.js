69b08ea1f0b23e349d63b3686f20af8bd9fa3e2f
AzharMoosa <azhar.moosa8@gmail.com>
2020-07-23 10:56:11 +0100

    Login Page

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { Fragment } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Navbar from "./components/layout/Navbar";
+import Home from "./components/pages/Home";
+import Login from "./components/auth/Login";
+import Register from "./components/auth/Register";
 import "./App.css";
 
 function App() {
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <Router>
+      <Fragment>
+        <Navbar />
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/register' component={Register} />
+          <Route exact path='/login' component={Login} />
+        </Switch>
+      </Fragment>
+    </Router>
   );
 }
 
---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,72 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <nav className='navbar bg-secondary'>
+      <div className='navbar-container'>
+        <div className='nav-right'>
+          <svg
+            className='logo'
+            xmlns='http://www.w3.org/2000/svg'
+            width='67'
+            height='68'
+            viewBox='0 0 67 68'
+          >
+            <g id='Logo' transform='translate(-123 -34)'>
+              <rect
+                id='Rectangle_8'
+                data-name='Rectangle 8'
+                width='67'
+                height='67'
+                rx='12'
+                transform='translate(123 34)'
+                fill='#2b4141'
+              />
+              <g id='Group_1' data-name='Group 1' transform='translate(57 2)'>
+                <text
+                  id='B'
+                  transform='translate(113 76)'
+                  fill='#fffa80'
+                  fontSize='22'
+                  fontFamily='Poppins-Medium, Poppins'
+                  fontWeight='500'
+                >
+                  <tspan x='0' y='0'>
+                    B
+                  </tspan>
+                </text>
+                <text
+                  id='O'
+                  transform='translate(74 83)'
+                  fill='#fffa80'
+                  fontSize='49'
+                  fontFamily='Poppins-Medium, Poppins'
+                  fontWeight='500'
+                >
+                  <tspan x='0' y='0'>
+                    O
+                  </tspan>
+                </text>
+              </g>
+            </g>
+          </svg>
+          <h1 className='navbar-title'>Odin Book</h1>
+        </div>
+
+        <ul>
+          <li className='nav-links'>
+            <Link to='/login' className='nav-link'>
+              Login
+            </Link>
+            <Link to='/register' className='nav-link'>
+              Register
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
