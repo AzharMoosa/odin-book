782eaf315dc051bd0e66a6f21ed33221ee04c5de
AzharMoosa <azhar.moosa8@gmail.com>
2020-08-27 11:09:18 +0100

    Prepare for deploy

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const usersRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
 const postsRoute = require("./routes/posts");
 const connectDB = require("./config/db");
+const path = require("path");
 const app = express();
 
 // Connect DB
@@ -11,16 +12,19 @@ connectDB();
 // Inititalize Middleware
 app.use(express.json({ extended: true }));
 
-app.get("/", (req, res) => {
-  res.json({
-    msg: "Welcome To Odin Book API",
-  });
-});
-
 // Define Routes
 app.use("/api/users", usersRoute);
 app.use("/api/auth", authRoute);
 app.use("/api/posts", postsRoute);
+
+// Server Static Assests In Production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
 
 // Server Listen
 const PORT = process.env.PORT || 5000;
